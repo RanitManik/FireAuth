96af9ecd877c5653cffffd53f37b60b8395222af
RanitManik <138437760+RanitManik@users.noreply.github.com>
2024-07-01 00:59:22 +0530

    fixed button component to be styled directly

--- src/component/auth_button/auth_button.component.jsx
+++ src/component/auth_button/auth_button.component.jsx
@@ -1,8 +1,8 @@
 // eslint-disable-next-line react/prop-types
 const AuthButton = ({ imgSrc, altText, buttonText }) => {
   return (
-    <button className="auth-btn">
-      <img src={imgSrc} alt={altText} />
+    <button className="flex gap-4 rounded-md border border-neutral-300 p-4 text-start text-gray-600 transition hover:border-neutral-400 hover:bg-gray-200 focus:border-neutral-500 focus:bg-gray-200 focus:outline-none active:border-neutral-600 active:bg-gray-100">
+      <img className="h-6 w-6" src={imgSrc} alt={altText} />
       {buttonText}
     </button>
   );
