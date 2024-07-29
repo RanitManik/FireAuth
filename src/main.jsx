a7c073f45feb2f2c164dd7bbd0084d9c8d27c018
RanitManik <138437760+RanitManik@users.noreply.github.com>
2024-07-29 23:59:16 +0530

    deleted react buddy plugin

--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { DevSupport } from "@react-buddy/ide-toolbox";
-import { ComponentPreviews, useInitial } from "./dev/index.js";
 import { FirebaseProvider } from "./context/firebase.context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <FirebaseProvider>
-        <DevSupport
-          ComponentPreviews={ComponentPreviews}
-          useInitialHook={useInitial}
-        >
-          <App />
-        </DevSupport>
+        <App />
       </FirebaseProvider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
